1721285328ec360ee6840035b4e96c46e838ceca
cleiton <cleitonjrti@gmail.com>
2019-10-25 16:10:05 -0300

    Added header to ChatScreen

--- mobile/src/screens/ChatScreen.js
+++ mobile/src/screens/ChatScreen.js
@@ -1,16 +1,20 @@
 import React, { useState } from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
 import MessageList from '../components/MessageList';
+import useDevSample from '../hooks/useDevSample';
 import useChatSample from '../hooks/useChatSample';
 import {
   SafeAreaView,
   View,
+  Text,
   TouchableOpacity,
   TextInput,
+  Image,
   StyleSheet
 } from 'react-native';
 
 export default function ChatScreen() {
+  const dev = useDevSample()[0];
   const [chatInputValue, setChatInputValue] = useState('');
   const [chatHistory, setChatHistory] = useState(useChatSample());
 
@@ -27,6 +31,19 @@ export default function ChatScreen() {
 
   return (
     <SafeAreaView style={styles.rootContainer}>
+      <View style={styles.header}>
+        <Image style={styles.headerImage} source={{uri: dev.image}} />
+
+        <View>
+          <Text style={styles.devName}>{dev.name}</Text>
+          <View style={styles.horizontalContainer}>
+            <Text style={styles.devUsername}> {dev.username}</Text>
+            <Icon style={styles.githubLogo} name="logo-github" size={16} />
+          </View>
+        </View>
+
+      </View>
+
       <MessageList data={chatHistory} />
 
       <View style={styles.chatInputContainer}>
@@ -73,5 +90,35 @@ const styles = StyleSheet.create({
   emptyChat: {
     color: '#666',
     textAlign: 'center',
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 5,
+    borderBottomWidth: 1,
+    borderBottomColor: '#eee',
+  },
+  headerImage: {
+    height: 50,
+    width: 50,
+    borderRadius: 10,
+    marginRight: 10,
+    borderWidth: 1.5,
+    borderColor: 'lightgreen',
+  },
+  devName: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  devUsername: {
+    color: '#999',
+    fontStyle: 'italic',
+  },
+  githubLogo: {
+    marginLeft: 1,
+  },
+  horizontalContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
   }
 });
