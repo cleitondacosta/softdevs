4c6246ec724bef2b1db08cc5546539d9c4393977
cleiton <cleitonjrti@gmail.com>
2019-11-06 11:55:38 -0300

    ChatHeader it's more general, since companies doesn't have image and name

--- mobile/src/components/ChatHeader.js
+++ mobile/src/components/ChatHeader.js
@@ -1,51 +1,29 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
-export default function ChatHeader({ receiver }) {
+export default function ChatHeader({ receiverName, senderName }) {
   return (
     <View style={styles.header}>
-      <Image style={styles.headerImage} source={{uri: receiver.image}} />
-
-      <View>
-        <Text style={styles.name}>{receiver.name}</Text>
-        <View style={styles.horizontalContainer}>
-          <Text style={styles.username}> {receiver.username}</Text>
-        </View>
-      </View>
-
+        <Text>
+          Logged as: <Text style={styles.name}>{senderName}</Text>
+        </Text>
+        <Text>
+          Chatting with: <Text style={styles.name}>{receiverName}</Text>
+        </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   header: {
-    flexDirection: 'row',
     alignItems: 'center',
     padding: 5,
     borderBottomWidth: 1,
     borderBottomColor: '#eee',
   },
-  headerImage: {
-    height: 50,
-    width: 50,
-    borderRadius: 10,
-    marginRight: 10,
-    borderWidth: 1.5,
-    borderColor: 'lightgreen',
-  },
   name: {
     fontWeight: 'bold',
+    fontStyle: 'italic',
     fontSize: 16,
   },
-  username: {
-    color: '#999',
-    fontStyle: 'italic',
-  },
-  githubLogo: {
-    marginLeft: 1,
-  },
-  horizontalContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  }
 });
--- mobile/src/screens/ChatScreen.js
+++ mobile/src/screens/ChatScreen.js
@@ -30,7 +30,7 @@ export default function ChatScreen() {
 
   return (
     <SafeAreaView style={styles.rootContainer}>
-      <ChatHeader receiver={dev} />
+      <ChatHeader receiverName={dev.username} senderName="company1" />
       <MessageList data={chatHistory} />
 
       <View style={styles.chatInputContainer}>
